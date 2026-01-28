4b66f2e496e03308006bbfe1d8a9800fe8c5d675
John Reynolds <john.reynolds@mullen.pvt.k12.co.us>
2026-01-28 14:59:24 -0700

    Initial Commit 1/28/2026

---
+++ src/TakeStudentIndex.java
@@ -0,0 +1,12 @@
+
+public class TakeStudentIndex
+	{
+
+		public static void main(String[] args)
+			{
+				System.out.println("Take students from .txt file project");
+				System.out.println("take input that grabs student information and pulls it");
+				// 1/28/2026 In Class Code
+			}
+
+	}
